8f0c035feae113299210710125a9d6dff27c406f
Roshan Patel <roshanpatel06@gmail.com>
2015-09-20 10:16:15 -0400

    updated input checker

--- inputcheck.c
+++ inputcheck.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
 
 	for(i=0;i<argc;i++){
 
-		printf("\ncmd line arg(%d) is: %s, \tlength %d\n",i,argv[i],(int)strlen(argv[i]));
+		printf("\ncmd line arg(%d) is: %s, \t\t Hex: '%x' \t\tlength %d\n",i,argv[i],argv[i],(int)strlen(argv[i]));
 	}
 	
   return 0;
